ac64518d57a6b7f2fc8ffdd11e71d1f3e58d78da
Goutham Kannan <54542723+GouthamKannan@users.noreply.github.com>
2023-05-03 10:29:35 +0530

    Update backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 var configs = {
-    api_url : "http://www.book-store.somee.com"
+    // api_url : "http://www.book-store.somee.com"
+    api_url : "http://www.books-store.somee.com"
 }
 
 export default configs
